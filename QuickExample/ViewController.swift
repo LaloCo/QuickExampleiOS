c0ef79038f8011515a07e4c1b0046bccea1a3fb8
LaloCo <lalorosas@outlook.com>
2019-04-10 15:39:56 -0500

    Initial Commit

---
+++ QuickExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QuickExample
+//
+//  Created by Eduardo Rosas on 4/10/19.
+//  Copyright © 2019 Eduardo Rosas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
